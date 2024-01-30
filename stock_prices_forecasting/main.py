459f482ed4c78f033315e8759461238ba7e82336
Dominik <nik.wrzesien@gmail.com>
2024-01-30 04:16:32 +0100

    end of tut

--- stock_prices_forecasting/main.py
+++ stock_prices_forecasting/main.py
@@ -2,14 +2,17 @@ import pandas as pd
 import datetime
 import matplotlib.pyplot as plt
 import numpy as np
+from keras.models import Sequential, load_model
+from keras.callbacks import ModelCheckpoint
+from keras.optimizers import Adam
+from keras import layers
+from copy import deepcopy
 
 df = pd.read_csv('KWHIY.csv')
 df = df[['Date', 'Close']]
 df['Date'] = pd.to_datetime(df['Date'])
 df.index = df.pop('Date')
 
-
-#
 # plt.plot(df.index, df['Close'])
 # plt.show()
 
@@ -66,8 +69,125 @@ def df_to_windowed_df(dataframe, first_date_str, last_date_str, n=3):
 
 
 windowed_df = df_to_windowed_df(df,
-                                '2020-03-25',
-                                '2022-03-23',
+                                '2014-05-30',
+                                '2023-12-27',
                                 n=3)
-
 print(windowed_df)
+
+def windowed_df_to_date_X_Y(windowed_dataframe):
+    df_as_np = windowed_dataframe.to_numpy()
+    dates = df_as_np[:, 0]
+
+    middle_matrix = df_as_np[:, 1:-1]
+    X = middle_matrix.reshape((len(dates), middle_matrix.shape[1], 1))
+    Y = df_as_np[:, -1]
+
+    return dates, X.astype(np.float32), Y.astype(np.float32)
+
+
+dates, X, y = windowed_df_to_date_X_Y(windowed_df)
+
+# # print(dates.shape)
+# # print(X.shape)
+# # print(y.shape)
+#
+q_80 = int(len(dates) * .8)
+q_90 = int(len(dates) * .9)
+
+dates_train, X_train, y_train = dates[:q_80], X[:q_80], y[:q_80]
+dates_val, X_val, y_val = dates[q_80:q_90], X[q_80:q_90], y[q_80:q_90]
+dates_test, X_test, y_test = dates[q_90:], X[q_90:], y[q_90:]
+#
+# # plt.plot(dates_train, y_train)
+# # plt.plot(dates_val, y_val)
+# # plt.plot(dates_test, y_test)
+# #
+# # plt.legend(['Train', 'Validation', 'Test'])
+# # plt.show()
+#
+# # Model creation and training
+#
+# # model = Sequential([
+# #     layers.Input((3, 1)),
+# #     layers.LSTM(64),
+# #     layers.Dense(32, activation='relu'),
+# #     layers.Dense(32, activation='relu'),
+# #     layers.Dense(1)
+# # ])
+# #
+# # model.compile(
+# #     loss='mse',
+# #     optimizer=Adam(learning_rate=0.001),
+# #     metrics=['mean_absolute_error']
+# # )
+# #
+# # cp = ModelCheckpoint('model/', save_best_only=True)
+# #
+# # model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=100, callbacks=[cp])
+# #
+model = load_model('model/')
+# #
+train_predictions = model.predict(X_train).flatten()
+# #
+# # plt.plot(dates_train, train_predictions)
+# # plt.plot(dates_train, y_train)
+# # plt.legend(['Training Predictions', 'Training Observations'])
+#
+val_predictions = model.predict(X_val).flatten()
+# #
+# # plt.plot(dates_val, val_predictions)
+# # plt.plot(dates_val, y_val)
+# # plt.legend(['Validation Predictions', 'Validation Observations'])
+#
+test_prediction = model.predict(X_test).flatten()
+#
+# # plt.plot(dates_test, test_prediction)
+# # plt.plot(dates_test, y_test)
+# # plt.legend(['Testing Predictions', 'Testing Observations'])
+# #
+# # # plt.plot(dates_train, train_predictions)
+# # # plt.plot(dates_train, y_train)
+# # # plt.plot(dates_val, val_predictions)
+# # # plt.plot(dates_val, y_val)
+# # # plt.plot(dates_test, test_prediction)
+# # # plt.plot(dates_test, y_test)
+# # #
+# # # plt.legend([
+# # #     'Training Predictions',
+# # #     'Training Observations',
+# # #     'Validation Predictions',
+# # #     'Validation Observations',
+# # #     'Testing Predictions',
+# # #     'Testing Observations',
+# # # ])
+# #
+# Prediction
+
+recursive_predictions = []
+recursive_dates = np.concatenate([dates_val, dates_test])
+
+for target_date in recursive_dates:
+    last_window = deepcopy(X_train[-1])
+    next_prediction = model.predict(np.array([last_window])).flatten()
+    recursive_predictions.append(next_prediction)
+    last_window[-1] = next_prediction
+
+plt.plot(dates_train, train_predictions)
+plt.plot(dates_train, y_train)
+plt.plot(dates_val, val_predictions)
+plt.plot(dates_val, y_val)
+plt.plot(dates_test, test_prediction)
+plt.plot(dates_test, y_test)
+plt.plot(recursive_dates, recursive_predictions)
+
+plt.legend([
+    'Training Predictions',
+    'Training Observations',
+    'Validation Predictions',
+    'Validation Observations',
+    'Testing Predictions',
+    'Testing Observations',
+    'Recursive Predictions'
+])
+
+plt.show()
